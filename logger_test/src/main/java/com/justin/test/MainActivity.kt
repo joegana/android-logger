4eaabc568b8fe8a36341e090a5408ed2d347a616
Justin <joegana@126.com>
2024-03-29 08:43:12 +0800

    logger 日志框架维护,文件本地日志做异步打印

--- logger_test/src/main/java/com/justin/test/MainActivity.kt
+++ logger_test/src/main/java/com/justin/test/MainActivity.kt
@@ -3,23 +3,47 @@ package com.justin.test
 import android.app.Activity
 import android.os.Bundle
 import com.justin.timber.Timber
-import com.noveogroup.android.log.LoggerManager
+import com.moorgen.android.log.LoggerManager
+import com.moorgen.android.log4j.LogConfigurator
+import org.slf4j.Logger
+import java.io.File
 
 /**
  * Created by admin on 2018/7/23.
  */
 class MainActivity:Activity() {
     private val Log = LoggerManager.getLogger(MainActivity::class.java)
+    private var logger:Logger? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        configLog4Android(true)
         Log.i("%s","就")
 //        Timber.plant(Timber.DebugTree())
         Timber.d("Timer log 测试")
-        Log.d("%就")
-        Log.v("%就")
-        Log.w("%就")
+        Log.d("%d",1)
+        Log.v("%s",1)
+        Log.w("%b",0)
         Log.e("%就")
         Log.a("%就")
+    }
+
+
+    private fun configLog4Android(enableDiskLog: Boolean) {
+        val logConfigurator = LogConfigurator()
+        val file = File(this.externalCacheDir,"test.log")
+        val path = file.path
+        logConfigurator.setFileName(path)
+        logConfigurator.setRootLevel(org.apache.log4j.Level.ALL)
+        logConfigurator.setLevel("org.apache", org.apache.log4j.Level.ALL)
+        logConfigurator.setUseFileAppender(enableDiskLog)
+        logConfigurator.setFilePattern("%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p -[%t] [%c{2}]-[%L] %m%n")
+        logConfigurator.setLogCatPattern("%d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c{2}]-[%L] %m%n")
+        logConfigurator.setMaxBackupSize(21)
+        logConfigurator.setMaxFileSize(1024 * 1024 * 2)
+        logConfigurator.setImmediateFlush(true)
+        logConfigurator.configure()
+        logger = org.slf4j.LoggerFactory.getLogger(MainActivity::class.java)
+        logger?.info("LogFilePath = {} ”", path)
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
